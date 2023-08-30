b10a2de4c9af723e04207cb82c4c676ae3a8847b
Niharsh <93673801+Niharsh@users.noreply.github.com>
2023-08-30 08:03:51 +0530

    Time: 348 ms (26.49%), Space: 10.2 MB (60.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,8 +4,7 @@ public:
         int n=nums.size();
         vector <int> result;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i==j)continue;
+            for(int j=i+1;j<n;j++){
                 if(nums[i]+nums[j]==target){
                     result.push_back(i);
                     result.push_back(j);
